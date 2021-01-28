b06168c75712e4410368db39bd46d24bcd19ee13
Mark Curran <S00199895@mail.itsligo.ie>
2021-01-28 15:44:11 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace S00199895_Mark_Curran_Book_App
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
