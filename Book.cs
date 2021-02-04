39a3e6cd817bf3ac0129c37761ee5d99baebce89
Mark Curran <S00199895@mail.itsligo.ie>
2021-02-04 11:04:54 +0000

    Created Book.cs

---
+++ Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S00199895_Mark_Curran_Book_App
+{
+	public class Book
+	{
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public int Rating { get; set; }
+		public string ImageURI { get; set; }
+	}
+
+}
