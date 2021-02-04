93c0e0b9862a9daf85815923eb8d9a775ce8d06c
Mark Curran <S00199895@mail.itsligo.ie>
2021-02-04 11:30:21 +0000

    BookTBR : Book.cs

--- Book.cs
+++ Book.cs
@@ -8,10 +8,25 @@ namespace S00199895_Mark_Curran_Book_App
 {
 	public class Book
 	{
+		//props
 		public string Title { get; set; }
 		public string Author { get; set; }
+		public string Description { get; set; }
 		public int Rating { get; set; }
 		public string ImageURI { get; set; }
+
+		//ctor
+		public Book(string title, string author, int rating)
+		{
+			Title = title;
+			Author = author;
+			Rating = rating;
+		}
+		public Book(string title, string author)
+		{
+			Title = title;
+			Author = author;
+		}
 	}
 
 }
---
+++ BookTBR.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S00199895_Mark_Curran_Book_App
+{
+	public class BookTBR : Book
+	{
+		//props
+		public DateTime DateRead { get; set; }
+
+		//inherits base class Book
+		public BookTBR(string title, string author) : base(title, author)
+		{
+
+		}
+	}
+}
